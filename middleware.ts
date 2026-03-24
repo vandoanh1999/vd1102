7a3eaa3c03bdaf4a8e9afa4b28529971d78fd9f1
vandoanh1999 <phamvandoanh9@gmail.com>
2026-03-24 10:22:31 +0700

    fix: thêm cookie name NextAuth v5

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,9 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const isLogged =
     req.cookies.get("next-auth.session-token") ||
-    req.cookies.get("__Secure-next-auth.session-token");
+    req.cookies.get("__Secure-next-auth.session-token") ||
+    req.cookies.get("authjs.session-token") ||
+    req.cookies.get("__Secure-authjs.session-token");
   const isLogin = req.nextUrl.pathname.startsWith("/login");
   if (!isLogged && !isLogin) {
     return NextResponse.redirect(new URL("/login", req.url));
